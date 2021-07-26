06ddb6efe219501b80a6d93f87aa8aa6b739cb7c
Ritwik_Chatterjee <ritwikawesome556@gmail.com>
2021-07-26 13:17:23 +0530

    Create evaluate_prefix.c

---
+++ evaluate_prefix.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+#define MAX 100
+
+float stack[MAX];
+int top=-1;
+char prefix[MAX];
+
+int main(){
+
+    
+}
